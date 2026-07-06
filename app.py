6a98a5700560adfc20c3e532e2d3da6fe587a4d3
KDarbha19 <Kausthubhd@gmail.com>
2026-07-06 18:10:51 -0400

    quick fixes to ensure it works

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 #Load model once when the server starts- not every req
 device = torch.device("mps") if torch.backends.mps.is_available() else torch.device("cpu")
 print(f"Loading model on {device}")
-tokenizer = T5Tokenizer.from_pretrained("model/final").to(device)
+tokenizer = T5Tokenizer.from_pretrained("model/final")
 model = T5ForConditionalGeneration.from_pretrained("model/final").to(device)
 model.eval() #puts model in inference mode, disables dropout
 print("Model loaded")
